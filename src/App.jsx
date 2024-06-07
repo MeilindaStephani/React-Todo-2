e308676c0be2f9e41cebb4e96741359c90997efa
MeilindaStephani <meilindastephaniee@gmail.com>
2024-06-07 20:26:26 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from "react";
+import Todos from "./components/Todos";
+import TodoForm from "./components/TodoForm";
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "Finish Progate React Course",
+      completed: false,
+      completedAt: null,
+    },
+    {
+      id: 2,
+      title: "Have lunch with Guru Domba",
+      completed: false,
+      completedAt: null,
+    },
+    {
+      id: 3,
+      title: "Study React with Ninja Ken",
+      completed: false,
+      completedAt: null,
+    },
+  ]);
+
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  const toggleCompleted = (todoId) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === todoId) {
+        const completedAt = todo.completed ? null : new Date();
+        return { ...todo, completed: !todo.completed, completedAt };
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  const deleteTodo = (todoId) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== todoId);
+    setTodos(updatedTodos);
+  };
+
+  const addTodo = (todoTitle) => {
+    if (todoTitle === "") {
+      return;
+    }
+
+    const newTodo = {
+      id: todos.length + 1,
+      title: todoTitle,
+      completed: false,
+      completedAt: null,
+    };
+
+    const updatedTodos = todos.concat(newTodo);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div className="text-center p-3 my-5">
+      <h1 className="text-3xl font-bold">My Todo List</h1>
+      <p className="text-2xl mb-5">{currentTime.toLocaleTimeString("default", { hour12: false })}</p>
+      <TodoForm addTodo={addTodo} />
+      <Todos todos={todos} toggleCompleted={toggleCompleted} deleteTodo={deleteTodo} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const TodoForm = ({ addTodo }) => {
+  // Definisikan state "title"
+  const [title, setTitle] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addTodo(title); // Meneruskan title sebagai parameter ke addTodo
+    setTitle(""); // Reset title-nya
+  };
+
+  // Definisikan function "handleChangeTitle"
+  const handleChangeTitle = (event) => {
+    setTitle(event.target.value);
+  };
+
+  // Periksa apakah function "handleChangeTitle" berfungsi
+  console.log(title);
+
+  return (
+    <div className="mb-8">
+      <form
+        onSubmit={(event) => {
+          handleSubmit(event);
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Add your Todo"
+          className="w-4/5 h-16 text-base px-4 mx-4"
+          onChange={(event) => {
+            handleChangeTitle(event);
+          }}
+          value={title}
+        />
+        <button className="w-1/7 h-18 text-base bg-blue-600 text-white px-4 py-5 rounded">Add Todo</button>
+      </form>
+    </div>
+  );
+};
+
+TodoForm.propTypes = {
+  addTodo: PropTypes.func.isRequired,
+};
+
+export default TodoForm;
